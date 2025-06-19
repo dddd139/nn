313e0071112f0fbb396c64094f3588c77e6d5e15
dddd139 <dmitrijbondar326@gmail.com>
2025-06-19 20:47:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -149,4 +149,16 @@ async def main():
 
 if __name__ == "__main__":
     download_database()
-    asyncio.run(main())
+
+    try:
+        import uvloop
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    except ImportError:
+        pass
+
+    async def runner():
+        await main()
+
+    asyncio.get_event_loop().create_task(runner())
+    asyncio.get_event_loop().run_forever()
+
