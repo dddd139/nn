d251f8f8bfe0ca13aeb14d406d80802f59473e4c
dddd139 <dmitrijbondar326@gmail.com>
2025-06-19 18:23:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -182,4 +182,13 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        if "event loop is already running" in str(e):
+            loop = asyncio.get_event_loop()
+            loop.create_task(main())
+            loop.run_forever()
+        else:
+            raise
+
